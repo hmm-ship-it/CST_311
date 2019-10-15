543c31588750584795909f430391a8da74768cc3
Federico Rubino <52460126+FedericoRubino@users.noreply.github.com>
2019-10-15 07:42:05 -0700

    Update TCP_Client.py

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,11 +1,31 @@
-from socket import *
-serverName = ’servername’
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = raw_input(‘Input lowercase sentence:’)
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print (‘From Server:’, modifiedSentence.decode())
-clientSocket.close()
+"""
+Tim Hanneman
+Federico Rubino
 
+Programming Assignment #2
+
+"""
+
+# In your command prompt, type in hostname and press enter.
+# What comes up is your computer's hostname
+
+
+from socket import *
+
+serverName = 'localhost'
+
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+clientSocket.connect((serverName,serverPort))
+
+sentence = input('Input lowercase sentence:')
+
+clientSocket.send(sentence.encode())
+
+modifiedSentence = clientSocket.recv(1024)
+
+print ('From Server:', modifiedSentence.decode())
+
+clientSocket.close()
