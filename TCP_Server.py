d93243b06b67985d74161fc1fd3ba1beddc61a6e
Federico Rubino <52460126+FedericoRubino@users.noreply.github.com>
2019-10-15 08:11:27 -0700

    Update TCP_Server.py

--- TCP_Server.py
+++ TCP_Server.py
@@ -1,15 +1,50 @@
+"""
+Tim Hanneman
+Federico Rubino
+
+Assignment 2
+
+ServerTCP.py
+"""
+
+
+
 from socket import *
 serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind((‘’,serverPort))
-serverSocket.listen(1)
-print ‘The server is ready to receive’
-while True:
-     connectionSocket, addr = serverSocket.accept()
-     
-     sentence = connectionSocket.recv(1024).decode()
-     capitalizedSentence = sentence.upper()
-     connectionSocket.send(capitalizedSentence.
-                                                            encode())
-     connectionSocket.close()
+servername = '127.0.0.1'
 
+# Create a TCP socket
+# Notice the use of SOCK_STREAM for TCP packets
+serverSocket = socket(AF_INET,SOCK_STREAM)
+
+# Assign IP address and port number to socket
+serverSocket.bind((servername,serverPort))
+serverSocket.listen(1)
+
+print ('The server is ready to receive')
+
+while True:
+
+     connectionSocket, addr = serverSocket.accept()
+
+     sentence = connectionSocket.recv(1024).decode()
+
+
+     # this checks for different client messages
+     print (sentence)
+     new_message = ""
+
+     if 'X' in sentence:
+          new_message = sentence + " received before Y:Tim"
+     else:
+          new_message = sentence + " received before X:Federico"
+
+     connectionSocket.send(new_message.encode())
+
+
+
+#     capitalizedSentence = sentence.upper()
+
+#     connectionSocket.send(capitalizedSentence.encode())
+
+     connectionSocket.close()
